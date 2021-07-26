b08ba5fb368288e75dad2bfcf3d9cc9c49035712
Vidottizzz <vidottizzz@gmail.com>
2021-07-26 11:08:45 -0300

    Incluindo o restante das informações do API incluindo os icones

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ document.querySelector(".busca").addEventListener("submit",async (event)=>{
     let input = document.querySelector("#searchInput").value;
 
     if (input !== ''){
+        clearInfo();
         showWarning('Carregando...');
         //encodeURI para ficar no formato certo
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=51a5f0fa188b890457fb261ab22baa67&units=metric&lang=pt_br`;  
@@ -21,11 +22,17 @@ document.querySelector(".busca").addEventListener("submit",async (event)=>{
                 tempIcon : json.weather[0].icon,
                 windSpeed : json.wind.speed,
                 windAngle: json.wind.deg
-            })
+            });
         } else {
+            //limpar quando nao tiver nada
+            clearInfo();
             showWarning('Não encontramos esta localização.');
         }
-    }           
+        //limpar tudo caso nao procurar nada e der search
+    } else {
+        clearInfo();
+    }
+            
 });
 
 function showInfo(json) {
@@ -34,7 +41,15 @@ function showInfo(json) {
     document.querySelector('.resultado').style.display = 'block';
 
     document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
-
+    document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>`;
+    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`;
+    //procurar a imagem do icone
+    document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
+    document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle - 90}deg)`
+}
+function clearInfo(){
+    showWarning('');
+    document.querySelector('.resultado').style.display = 'none';
 }
 
 
